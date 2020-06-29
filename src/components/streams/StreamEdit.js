32452cfd37f68194a09d674eb750501a2c47113b
Valchee <matkowski2@gmail.com>
2020-06-30 00:21:09 +0200

    refactor StreamCreate and StreamEdit to be functional components

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,33 +1,32 @@
 import _ from "lodash";
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchStream, editStream } from "../../actions";
 import StreamForm from "./StreamForm";
 
-class StreamEdit extends React.Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+const StreamEdit = props => {
+
+  useEffect(() => {
+    props.fetchStream(props.match.params.id);
+  });
+
+  const onSubmit = (formValues) => {
+    props.editStream(props.match.params.id, formValues);
   }
 
-  onSubmit = (formValues) => {
-    this.props.editStream(this.props.match.params.id, formValues);
+  if (!props.stream) {
+    return <div>Loading...</div>;
   }
 
-  render() {
-    if (!this.props.stream) {
-      return <div>Loading...</div>;
-    }
-
-    return (
-      <div>
-        <h3>Edit a Stream</h3>
-        <StreamForm
-          onSubmit={this.onSubmit}
-          initialValues={_.pick(this.props.stream, 'title', 'description')}
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h3>Edit a Stream</h3>
+      <StreamForm
+        onSubmit={onSubmit}
+        initialValues={_.pick(props.stream, 'title', 'description')}
+      />
+    </div>
+  );
 }
 
 const mapStateToProps = (state, ownProps) => {
